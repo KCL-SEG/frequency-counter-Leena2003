4dd954d516152c7650055d7a448e0189deb946af
Leena2003 <99492322+Leena2003@users.noreply.github.com>
2022-10-22 23:53:15 +0100

    Dictionary updates based on each item

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for i in range(len(items)):
+        frequencies[str(items[i])] = 0
+
+    for j in range(len(items)):
+        frequencies[str(items[j])] += 1
+
     return frequencies
